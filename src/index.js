bf5a2e30dbf39a1d277605b424e745b3f40e8e84
Fernando Almeida <dealmeida.fernando96@yahoo.com.br>
2020-09-28 19:19:23 -0300

    feat: 🎸 daaaaaaa

--- src/index.js
+++ src/index.js
@@ -23,7 +23,9 @@ try {
   // const bla = octokit.teams.listChildInOrg('catho', '')
   // console.log('BLA ==>', bla);
   // octokit.teams.listMembersInOrg({ org: 'catho', team_slug: 'thunderbolts', }) .then(({ data }) => { console.log(data); })
-  const bla = octokit.repos.listTeams({ ...github.context.repo });
+  const bla = octokit.repos.listTeams({ ...github.context.repo }).then(() => {
+    console.log(bla);
+  });
   console.log(bla);
 
   const currentUser = contextPullRequest.user.login;
