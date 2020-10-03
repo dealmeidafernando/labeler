8e65b875d116d1a3d60ecee1857b2cd78a5eb2b6
Fernando Almeida <dealmeida.fernando96@yahoo.com.br>
2020-10-02 21:46:01 -0300

    feat: 🎸 fasdfsda

--- src/size.js
+++ src/size.js
@@ -61,10 +61,10 @@ async function size() {
   // const res = await context.github.pullRequests.listFiles({owner, repo, number})
 
   var res = await octokit.pulls.listFiles({ owner, repo, number }).catch((e) => { console.error(e.message) });
+  console.log("RES ==>", res);
   // const res = await octokit.pulls.listFiles({ owner, repo, number }).catch(error => { throw error});
 
-  console.log('PULL =>', pullRequest);
-  console.log(res);
+  // console.log('PULL =>', pullRequest);
 }
 
 module.exports = { size };
