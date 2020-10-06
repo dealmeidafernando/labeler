505ac88ab5a76f9050b52554e22c36b6f08cd892
Fernando Almeida <dealmeida.fernando96@yahoo.com.br>
2020-10-06 17:00:31 -0300

    fix: 🐛 switch

--- src/size.js
+++ src/size.js
@@ -34,7 +34,7 @@ const sizes = {
 };
 
 function sizeLabel(lineCount) {
-  switch (lineCount) {
+  switch (lineCount > 0) {
     case lineCount < sizes.S:
       return label.XS;
     case lineCount < sizes.M:
