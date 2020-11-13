a0a444c208fd31215efe0e49864b1996b415d145
Fernando Almeida <dealmeida.fernando96@yahoo.com.br>
2020-11-13 14:23:51 -0300

    fix: 🐛 change color

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const label = require('./label');
 
 const teamOne = 'teamOneMembers';
 const labelTeamOne = 'teamOneLabel';
-const colorTeamOne = '7c0dc1';
+const colorTeamOne = 'ffce00';
 
 // const teamTwo = 'teamTwoMembers';
 // const labelTeamTwo = 'teamTwoLabel';
